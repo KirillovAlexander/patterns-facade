64b5f065b717791e43db6ca555cc4fce8fbe53d7
Kirillov Alexander <kirillovbrest@gmail.com>
2021-02-02 22:14:33 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,9 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10110", "100"));
+        System.out.println(bins.mult("10", "110"));
+    }
+}
